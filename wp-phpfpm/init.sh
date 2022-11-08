8c10148caf391443aef2b869021a26f4305dbd76
Naerhy <naerhy@protonmail.com>
2022-11-09 00:59:59 +0100

    Fix WP installation PHP error

--- wp-phpfpm/init.sh
+++ wp-phpfpm/init.sh
@@ -2,10 +2,15 @@
 
 sleep 3
 
-echo "Generating configuration file for Wordpress!"
+echo "Generating configuration file for Wordpress..."
 wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_ADMIN \
 	--dbpass=$MARIADB_ADMIN_PASSWORD --dbhost=mariadb
 echo "Wordpress configuration file generated!"
 
-echo "Starting PHP-FPM!"
+echo "Installing Wordpress..."
+wp core install --url=localhost --title="Inception" --admin_user=$WP_ADMIN \
+	--admin_password=$WP_ADMIN_PASSWORD --admin_email=inception@student.42.fr --skip-email
+echo "Wordpress is installed!"
+
+echo "Starting PHP-FPM..."
 php-fpm7 -F -R
