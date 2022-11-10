2b4e7ca0093dc9af02f12c332613701ce544f168
Naerhy <naerhy@protonmail.com>
2022-11-10 18:00:31 +0100

    Modify ENV values

--- src/wordpress/init.sh
+++ src/wordpress/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-while ! mysql --user=$MARIADB_ADMIN --password=$MARIADB_ADMIN_PASSWORD -h mariadb --execute "SHOW DATABASES;"
+while ! mysql --user=$MARIADB_LOGIN --password=$MARIADB_PASSWORD -h mariadb --execute "SHOW DATABASES;"
 do
 	sleep 5
 done
@@ -8,14 +8,18 @@ done
 if [ ! -e wp-config.php ]
 then
 	echo "Generating configuration file for Wordpress..."
-	wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_ADMIN \
-		--dbpass=$MARIADB_ADMIN_PASSWORD --dbhost=mariadb
+	wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_LOGIN \
+		--dbpass=$MARIADB_PASSWORD --dbhost=mariadb
 	echo "Wordpress configuration file generated!"
 
 	echo "Installing Wordpress..."
-	wp core install --url=localhost --title="Inception" --admin_user=$WP_ADMIN \
-		--admin_password=$WP_ADMIN_PASSWORD --admin_email=inception@student.42.fr --skip-email
+	wp core install --url=localhost --title="Inception" --admin_user=$WP_ADMIN_LOGIN \
+		--admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
 	echo "Wordpress is installed!"
+
+	echo "Creating second user.."
+	wp user create $WP_AUTHOR_LOGIN $WP_AUTHOR_EMAIL --user_pass=$WP_AUTHOR_PASSWORD --role=author
+	echo "Second user created!"
 
 	echo "Installing Wordpress theme..."
 	wp theme install twentytwentyone --activate
